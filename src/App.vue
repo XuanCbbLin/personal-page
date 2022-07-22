9536d585e9b8027176cae0826fcac834659fbbd1
xuan <xuancbblin@gmail.com>
2022-07-22 19:17:56 +0800

    feat: banner 切版

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Banner></Banner>
+</template>
+
+<script setup>
+import Banner from "./components/Banner.vue";
+</script>
+
+<style></style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="container">
+      <div>
+        <div>Here’s</div>
+        <div>JOANNE</div>
+        <p>Founder of “XXXD Official’’ & Graphic Designer</p>
+        <ul>
+          <li><a href="#"> </a></li>
+          <li><a href="#"> </a></li>
+          <li><a href="#"> </a></li>
+          <li><a href="#"> </a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
