34ea9ff304897d6cf96e64a2c248a7cf48dc468c
xuan <xuancbblin@gmail.com>
2022-07-25 22:27:05 +0800

    feat: 新增 work

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <Banner></Banner>
   <About></About>
   <Brand></Brand>
+  <Work></Work>
 </template>
 
 <script setup>
 import Banner from "./components/Banner.vue";
 import About from "./components/About.vue";
 import Brand from "./components/Brand.vue";
+import Work from "./components/Work.vue";
 </script>
 
 <style></style>
---
+++ src/components/Work.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="pt-14 pb-36">
+    <div class="container w-[1200px] mx-auto">
+      <div class="text-center mb-14">
+        <h2 class="text-[64px] mb-4">Other Works</h2>
+        <p class="text-xl">2016-2020 年累積的作品 海報/廣告/攝影/LOGO/3D</p>
+      </div>
+      <div class="flex flex-wrap">
+        <div class="w-96 text-center">
+          <img src="../assets/images/work_img1.png" alt="work_img1" class="mb-4" />
+          <div class="text-xl">海報設計/2017</div>
+        </div>
+        <div class="w-96 ml-6 text-center">
+          <img src="../assets/images/work_img2.png" alt="work_img2" class="mb-4" />
+          <div class="text-xl">海報設計/2018</div>
+        </div>
+        <div class="w-96 ml-6 text-center">
+          <img src="../assets/images/work_img3.png" alt="work_img3" class="mb-4" />
+          <div class="text-xl">海報設計/2018</div>
+        </div>
+        <div class="w-[588px] text-center py-12">
+          <img src="../assets/images/work_img4.png" alt="work_img4" class="mb-4" />
+          <div class="text-xl">概念攝影/2018</div>
+        </div>
+        <div class="w-[588px] ml-6 text-center py-12">
+          <img src="../assets/images/work_img5.png" alt="work_img5" class="mb-4" />
+          <div class="text-xl">概念攝影/2018</div>
+        </div>
+        <div class="w-96 text-center">
+          <img src="../assets/images/work_img6.png" alt="work_img6" class="mb-4" />
+          <div class="text-xl">sasa 酒標設計/2019</div>
+        </div>
+        <div class="w-96 ml-6 text-center">
+          <img src="../assets/images/work_img7.png" alt="work_img7" class="mb-4" />
+          <div class="text-xl">攝影創作/2018</div>
+        </div>
+        <div class="w-96 ml-6 text-center">
+          <img src="../assets/images/work_img8.png" alt="work_img8" class="mb-4" />
+          <div class="text-xl">官網設計/2020</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
