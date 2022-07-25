942dbac4159e95c6ee4c852531c5b94ac31edd8f
xuan <xuancbblin@gmail.com>
2022-07-25 18:59:45 +0800

    feat: 新增brand 區塊

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Banner></Banner>
   <About></About>
+  <Brand></Brand>
 </template>
 
 <script setup>
 import Banner from "./components/Banner.vue";
 import About from "./components/About.vue";
+import Brand from "./components/Brand.vue";
 </script>
 
 <style></style>
---
+++ src/components/Brand.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="bg-dark-900 pt-14 pb-40">
+    <div class="container w-[1200px] mx-auto">
+      <div class="text-white">
+        <h2 class="text-[64px]">My Brand ‘’XXXD’’</h2>
+        <h3 class="text-2xl">Just Keep XXXD Everyday</h3>
+        <p class="text-xl">2020 年創立的個人服飾品牌 引領叛逆少年開啟與社會對話的窗口 主打概念潮流T-Shirt</p>
+      </div>
+      <div>
+        <div>
+          <img src="../assets/images/brand_img1.png" alt="Brand1" />
+        </div>
+        <div>
+          <img src="../assets/images/brand_img2.png" alt="Brand2" />
+        </div>
+        <div>
+          <img src="../assets/images/brand_img3.png" alt="Brand3" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
