7ff37c859ec7caef3af45b8522e5041d6b0ebdba
xuan <xuancbblin@gmail.com>
2022-07-25 11:47:28 +0800

    feat: banner區塊完成

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,12 +4,20 @@
       <div class="text-white w-[300px]">
         <div>Here’s</div>
         <div class="text-[80px]">JOANNE</div>
-        <p class="text-2xl">Founder of “XXXD Official’’ & Graphic Designer</p>
-        <ul>
-          <li><a href="#"> </a></li>
-          <li><a href="#"> </a></li>
-          <li><a href="#"> </a></li>
-          <li><a href="#"> </a></li>
+        <p class="text-2xl mb-6">Founder of “XXXD Official’’ & Graphic Designer</p>
+        <ul class="flex">
+          <li>
+            <a href="#" class="inline-block"><img src="../assets/icons/iconfinder_6_social_2609577.png" alt="Be" /> </a>
+          </li>
+          <li>
+            <a href="#" class="inline-block"> <img src="../assets/icons/iconfinder_1_social_2609541.png" alt="fb" /> </a>
+          </li>
+          <li>
+            <a href="#" class="inline-block"><img src="../assets/icons/iconfinder_25_social_2609558.png" alt="ig" /> </a>
+          </li>
+          <li>
+            <a href="#" class="inline-block"><img src="../assets/icons/iconfinder_6_social_2609577.png" alt="youtube" /> </a>
+          </li>
         </ul>
       </div>
     </div>
@@ -21,5 +29,11 @@
 <style scoped lang="scss">
 .banner {
   background: url("../assets/images/banner.png") no-repeat;
+
+  li {
+    ~ li {
+      margin-left: 20px;
+    }
+  }
 }
 </style>
