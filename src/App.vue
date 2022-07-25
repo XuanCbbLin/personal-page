23631bf8a966925b21c8823aed4a155c3295a356
xuan <xuancbblin@gmail.com>
2022-07-25 14:33:15 +0800

    feat: 新增about 區塊切版

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Banner></Banner>
+  <About></About>
 </template>
 
 <script setup>
 import Banner from "./components/Banner.vue";
+import About from "./components/About.vue";
 </script>
 
 <style></style>
---
+++ src/components/About.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="py-10">
+    <div class="container w-[1200px] mx-auto flex justify-center">
+      <div class="w-[384px] text-center">
+        <div>
+          <div class="text-[64px] mb-10">About</div>
+          <img src="../assets/images/photo.png" alt="about-me" />
+          <div class="pt-5 pb-6 border-b-[1px] border-black">
+            <h1 class="text-2xl mb-1">Joanne Chen</h1>
+            <div class="text-2xl mb-2">1997/10/10</div>
+            <p class="text-2xl">National Hex School Normal University Department of Visual Design</p>
+          </div>
+        </div>
+        <ul class="resume pt-7">
+          <li class="text-2xl"><span>Founder of ‘’XXXD Official’’</span></li>
+          <li class="text-2xl"><span>Graphic Designer</span></li>
+          <li class="text-2xl"><span>Graphic Intern of Hex School</span></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.resume {
+  li {
+    &:before {
+      content: "";
+      width: 8px;
+      height: 8px;
+      display: inline-block;
+      border-radius: 50%;
+      background-color: #891818;
+      margin-right: 10px;
+      vertical-align: middle;
+    }
+    ~ li {
+      margin-top: 28px;
+    }
+
+    span {
+      display: inline-block;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
